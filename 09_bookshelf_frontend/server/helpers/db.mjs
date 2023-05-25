47b33118c559e27ad310bb934a329d417f9e801a
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-05-25 16:09:42 -0400

    MongoDB Vercel test

--- 09_bookshelf_frontend/server/helpers/db.mjs
+++ 09_bookshelf_frontend/server/helpers/db.mjs
@@ -7,7 +7,32 @@ env.config();
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+import { MongoClient } from 'mongodb';
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Please add your Mongo URI to .env.local');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+export default async function handler(request, response) {
+  try {
+    const mongoClient = await new MongoClient(uri, options).connect();
+    console.log('Just Connected!');
+    const db = mongoClient.db('bookshelf');
+    const collection = db.collection('books');
+    const results = await collection
+      .find({})
+      .project({
+        title: '',
+        description: '',
+      })
+      .limit(4)
+      .toArray();
+
+    response.status(200).json(results);
+  } catch (e) {
+    response.status(500).json(e);
+  }
+}
