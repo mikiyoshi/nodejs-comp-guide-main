324df8b714e3d185c757cdb0e931138c6047f88d
mikiyoshi <mikiyoshi.kokura@gmail.com>
2023-05-26 17:56:10 -0400

    Update 08_bookshelf_backend MONGODB_URI

--- 08_bookshelf_backend/server/helpers/db.mjs
+++ 08_bookshelf_backend/server/helpers/db.mjs
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-import env from "dotenv";
+import env from 'dotenv';
 env.config();
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
